9846513eae61554196edc77daf71dfc63d39531f
zpg851105 <38696855+zpg851105@users.noreply.github.com>
2019-04-19 11:40:00 +0800

    Add files via upload

---
+++ AbstractConditionHandler.java
@@ -0,0 +1,25 @@
+package com.refordom.roleplay.base.service.condition;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+
+import java.util.Map;
+
+/**
+ * <p></p>
+ *
+ * @author 王晟权
+ * @date 2018/9/25 18:22
+ */
+public abstract class AbstractConditionHandler<T> implements ConditionProvider<T> {
+
+    @Override
+    public void opExecute(EntityWrapper<T> entityWrapper, Map<String, String[]> requestMap) {
+        if (isHandler(requestMap)) {
+            execute(entityWrapper, requestMap);
+        }
+    }
+
+    abstract boolean isHandler(Map requestMap);
+
+    abstract void execute(EntityWrapper<T> entityWrapper, Map requestMap);
+}
